35b8da6b7e29fe5759193c875cfc0da8e3b854ab
Talip Yıldız <talip.yildiz@kodiks.com>
2026-07-07 00:22:44 +0300

    fixed astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://talipyildiz.github.io',
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [
+      // @ts-expect-error Vite plugin types differ between Astro and @tailwindcss/vite
+      tailwindcss(),
+    ],
   },
 
   integrations: [react()]
